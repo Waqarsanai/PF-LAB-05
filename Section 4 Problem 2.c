f418f0941c33ed847030e8c5de2129bd9297300b
Waqar sanai <138406571+Waqarsanai@users.noreply.github.com>
2024-09-28 00:32:04 +0500

    Update Section 4 Problem 2.c

--- Section 4 Problem 2.c
+++ Section 4 Problem 2.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int number;
-    int count = 0;
+    int inputNumber, oneCount = 0;
 
-    printf("Enter a number: ");
-    scanf("%d", &number);
+    printf("Enter a positive integer: ");
+    scanf("%d", &inputNumber);
 
-    while (number > 0) {
-        count += number & 1;
-        number >>= 1;
-    }
+    if (inputNumber & 1) oneCount++;
+    if (inputNumber & 2) oneCount++;
+    if (inputNumber & 4) oneCount++;
+    if (inputNumber & 8) oneCount++;
+    if (inputNumber & 16) oneCount++;
+    if (inputNumber & 32) oneCount++;
+    if (inputNumber & 64) oneCount++;
+    if (inputNumber & 128) oneCount++;
 
-    printf("Number of 1s in the binary representation: %d\n", count);
+    printf("Number of 1s in the binary representation: %d\n", oneCount);
 
     return 0;
 }
