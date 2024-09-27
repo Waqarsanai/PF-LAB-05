566c7fdc8ee0e0bbedaab37d457b1572a51aa233
Waqar sanai <138406571+Waqarsanai@users.noreply.github.com>
2024-09-28 00:27:24 +0500

    Update Section 4 Problem 2.c

--- Section 4 Problem 2.c
+++ Section 4 Problem 2.c
@@ -1,1 +1,18 @@
- 
+#include <stdio.h>
+
+int main() {
+    int number;
+    int count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    while (number > 0) {
+        count += number & 1;
+        number >>= 1;
+    }
+
+    printf("Number of 1s in the binary representation: %d\n", count);
+
+    return 0;
+}
